2cb5ba4b20c2e5716de7d89ef1bf656ba96358fc
Elias <elias@mycarly.com>
2024-10-09 10:36:32 +0200

    ..

--- MauiApp1/Platforms/iOS/AppDelegate.cs
+++ MauiApp1/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,5 @@ namespace MauiApp1;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
